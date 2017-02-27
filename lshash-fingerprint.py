0ffcadb489213f526d4f00ad27db7a341e29adfa
Jamey Sparreboom <jamey@computerspar.nl>
2017-02-27 15:41:21 +0100

    Added script to test LShashing

---
+++ lshash-fingerprint.py
@@ -0,0 +1,8 @@
+from lshash import LSHash
+
+lsh = LSHash(6, 8)
+lsh.index([1, 2, 3, 4, 5, 6, 7, 8])
+lsh.index([2, 3, 4, 5, 6, 7, 8, 9])
+lsh.index([10, 12, 99, 1, 5, 31, 2, 3])
+
+print lsh.query([1,2,3,5,4,6,7,9])
