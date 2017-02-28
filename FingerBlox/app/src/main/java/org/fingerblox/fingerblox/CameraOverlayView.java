94b6dd88fa8d1c983ce8498df2fbfa34b341ffb0
jorenham <jhammudoglu@gmail.com>
2017-02-28 21:45:11 +0100

    Camera overlay

---
+++ FingerBlox/app/src/main/java/org/fingerblox/fingerblox/CameraOverlayView.java
@@ -0,0 +1,44 @@
+package org.fingerblox.fingerblox;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CameraOverlayView extends View {
+    private final static int PADDING = 150;
+
+    private Paint borderPaint = new Paint();
+    private Paint innerPaint = new Paint();
+    private RectF overlayRect = new RectF();
+
+    public CameraOverlayView(Context context) {
+        super(context);
+    }
+
+    public CameraOverlayView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CameraOverlayView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        borderPaint.setColor(Color.MAGENTA);
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(10);
+
+        innerPaint.setARGB(0, 0, 0, 0);
+        innerPaint.setStyle(Paint.Style.FILL);
+
+        overlayRect.set(PADDING, PADDING, getWidth() - PADDING, getHeight() - PADDING);
+
+        canvas.drawRect(overlayRect, innerPaint);
+        canvas.drawRect(overlayRect, borderPaint);
+    }
+}
