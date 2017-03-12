9252d71d7cb51154df4aac7216289f364bace570
jorenham <jhammudoglu@gmail.com>
2017-03-12 20:22:41 +0100

    Display image after capture bugfixes

--- FingerBlox/app/src/main/java/org/fingerblox/fingerblox/MainActivity.java
+++ FingerBlox/app/src/main/java/org/fingerblox/fingerblox/MainActivity.java
@@ -1,5 +1,8 @@
 package org.fingerblox.fingerblox;
 
+import android.content.Intent;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -43,6 +46,18 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         }
     };
 
+    private PictureCallback pictureCallback = new PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Log.i(TAG, String.format("Picture taken of %d bytes", data.length));
+            camera.startPreview();
+            camera.setPreviewCallback(mOpenCvCameraView);
+
+            Intent intent = new Intent(MainActivity.this, ImageDisplayActivity.class);
+            intent.putExtra("CAPTURED_IMAGE", data);
+            startActivity(intent);
+        }
+    };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +68,7 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         mOpenCvCameraView = (CameraView) findViewById(R.id.camera_preview);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+        mOpenCvCameraView.setPictureListener(pictureCallback);
 
         Button takePictureButton = (Button) findViewById(R.id.btn_takepicture);
         assert takePictureButton != null;
