3e06938920c65d16bd4811d53f6a8596acb76dbe
johannes.rauhamaa@iki.fi <Johannes Rauhamaa>
2017-03-14 16:08:28 +0100

    Do image processing in separate class. Add loading-dialog

--- FingerBlox/app/src/main/java/org/fingerblox/fingerblox/ImageDisplayActivity.java
+++ FingerBlox/app/src/main/java/org/fingerblox/fingerblox/ImageDisplayActivity.java
@@ -1,23 +1,11 @@
 package org.fingerblox.fingerblox;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 
-import org.opencv.android.Utils;
-import org.opencv.core.Core;
-import org.opencv.core.CvException;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
-import org.opencv.core.Scalar;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -36,63 +24,14 @@ public class ImageDisplayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image_display);
         mImageView = (ImageView) findViewById(R.id.image_view);
 
-        initializeImage();
-        displayImage();
+        Bundle bundle = getIntent().getExtras();
+        Bitmap bmp = (Bitmap) bundle.get("CAPTURED_IMAGE");
+        mImageView.setImageBitmap(bmp);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         this.finish();
-    }
-
-    protected void initializeImage() {
-        byte[] imageData = new byte[0];
-
-        Bundle bundle = getIntent().getExtras();
-        if (bundle != null) {
-            byte[] possibleImage = bundle.getByteArray("CAPTURED_IMAGE");
-            if (possibleImage != null) {
-                imageData = possibleImage;
-                Log.i(TAG, String.format("Picture received of %d bytes", imageData.length));
-            }
-        }
-
-        Mat BGRImage = Imgcodecs.imdecode(new MatOfByte(imageData), Imgcodecs.CV_LOAD_IMAGE_UNCHANGED);
-
-        // Convert BGR to Grayscale
-        image = emptyMat(BGRImage.cols(), BGRImage.rows());
-        Imgproc.cvtColor(BGRImage, image, Imgproc.COLOR_BGR2GRAY, 4);
-
-        image = rotateImage(image);
-    }
-
-    @NonNull
-    private Mat emptyMat(int width, int height) {
-        return new Mat(width, height, CvType.CV_8U, new Scalar(2));
-    }
-
-    protected void displayImage() {
-        // To rgba
-        Mat rgbaMat = new Mat(image.width(), image.height(), CvType.CV_8U, new Scalar(4));
-        Imgproc.cvtColor(image, rgbaMat, Imgproc.COLOR_GRAY2RGBA, 4);
-
-        // to bitmap
-        Bitmap bmp = Bitmap.createBitmap(rgbaMat.cols(), rgbaMat.rows(), Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(image, bmp);
-
-        mImageView.setImageBitmap(bmp);
-    }
-
-    /**
-     * OpenCV only supports landscape pictures, so we gotta rotate 90 degrees.
-     */
-    protected Mat rotateImage(Mat imageMat) {
-        Mat result = emptyMat(imageMat.rows(), imageMat.cols());
-
-        Core.transpose(imageMat, result);
-        Core.flip(result, result, 1);
-
-        return result;
     }
 }
--- FingerBlox/app/src/main/java/org/fingerblox/fingerblox/MainActivity.java
+++ FingerBlox/app/src/main/java/org/fingerblox/fingerblox/MainActivity.java
@@ -1,11 +1,15 @@
 package org.fingerblox.fingerblox;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Display;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
@@ -47,14 +51,32 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     };
 
     private PictureCallback pictureCallback = new PictureCallback() {
-        public void onPictureTaken(byte[] data, Camera camera) {
+        public void onPictureTaken(final byte[] data, Camera camera) {
             Log.i(TAG, String.format("Picture taken of %d bytes", data.length));
             camera.startPreview();
             camera.setPreviewCallback(mOpenCvCameraView);
 
-            Intent intent = new Intent(MainActivity.this, ImageDisplayActivity.class);
-            intent.putExtra("CAPTURED_IMAGE", data);
-            startActivity(intent);
+            final ProgressDialog progress = new ProgressDialog(MainActivity.this);
+            progress.setTitle("Loading");
+            progress.setMessage("Processing image...");
+            progress.setCancelable(false);
+            progress.show();
+
+            Thread mThread = new Thread() {
+                @Override
+                public void run() {
+                    Display display = getWindowManager().getDefaultDisplay();
+                    Point size = new Point();
+                    display.getSize(size);
+                    ImageProcessing p = new ImageProcessing(data);
+                    Bitmap bmp = p.getProcessedImage(size.x, size.y);
+                    progress.dismiss();
+                    Intent intent = new Intent(MainActivity.this, ImageDisplayActivity.class);
+                    intent.putExtra("CAPTURED_IMAGE", bmp);
+                    startActivity(intent);
+                }
+            };
+            mThread.start();
         }
     };
 
